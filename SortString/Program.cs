bdcec13360e4f5ada0e5c87c070f2561972e666e
bozsob <bozso.beatrix@gmail.com>
2017-05-31 12:27:36 +0200

    Exercise5: Working with string

--- SortString/Program.cs
+++ SortString/Program.cs
@@ -14,20 +14,9 @@ namespace SortString
             string str = "The dearest ones of time, the strongest friends of the soul--BOOKS. By Emily Dickinson";
             
             string[] sa = str.Split(' ');
-            foreach(string s in sa)
-            {
-                Console.WriteLine(s);
-            }
             
-
             Array.Sort(sa);
-            Console.WriteLine("after sorting:");
-            foreach (string s in sa)
-            {
-                Console.WriteLine(s);
-            }
-
-
+           
             str = string.Join(" ", sa);
             Console.WriteLine(str);
             Console.ReadKey();
