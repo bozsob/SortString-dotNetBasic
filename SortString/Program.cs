6ae1e32872b47d99d11c6042a3f4267e9cd9be63
bozsob <bozso.beatrix@gmail.com>
2017-05-31 12:06:20 +0200

    Add project files.

---
+++ SortString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortString
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string str = "The dearest ones of time, the strongest friends of the soul--BOOKS. By Emily Dickinson";
+            
+            string[] sa = str.Split(' ');
+            foreach(string s in sa)
+            {
+                Console.WriteLine(s);
+            }
+            
+
+            Array.Sort(sa);
+            Console.WriteLine("after sorting:");
+            foreach (string s in sa)
+            {
+                Console.WriteLine(s);
+            }
+
+
+            str = string.Join(" ", sa);
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
